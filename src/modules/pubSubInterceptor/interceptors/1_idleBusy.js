bc5a21a288ce787bf5aa6e9aef7364af5d9f826d
Mehmet Celik <mehmet@celik.be>
2023-09-14 05:26:50 +0200

    lirias

--- src/modules/pubSubInterceptor/interceptors/1_idleBusy.js
+++ src/modules/pubSubInterceptor/interceptors/1_idleBusy.js
@@ -6,11 +6,13 @@ pubSub.subscribe('before-pnxBaseURL', (reqRes) => {
     dotTestPerformanceStart = performance.now();
     document.querySelector('dot-test').innerHTML = '.'
     document.querySelector('dot-test').classList.add('busy')
+    document.querySelector('dot-test').classList.remove('performance')
     return reqRes;
 } );
 
 pubSub.subscribe('after-pnxBaseURL', (reqRes) => {
     document.querySelector('dot-test').classList.remove('busy')    
+    document.querySelector('dot-test').classList.add('performance')
     document.querySelector('dot-test').innerHTML = Math.ceil(performance.now() - dotTestPerformanceStart);    
     return reqRes;
 } );
